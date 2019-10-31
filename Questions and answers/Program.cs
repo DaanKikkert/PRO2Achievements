c00d475a34f7e47364e4d01c0e1f3929f5939bc4
DaanKikkert <54845837+DaanKikkert@users.noreply.github.com>
2019-10-31 22:30:39 +0100

    Soup
    a

---
+++ Questions and answers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Questions_and_answers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How are you today?");
+            string answer1 = Console.ReadLine();
+
+            Console.WriteLine("What did you do yesterday?");
+            string answer2 = Console.ReadLine();
+
+            Console.WriteLine("What have you got planned for the weekend?");
+            string answer3 = Console.ReadLine();
+            
+            Console.WriteLine("Your answers:");
+            Console.WriteLine("Q1. How are you today?: " + answer1);
+            Console.WriteLine("Q2. What did you do yesterday?: " + answer2);
+            Console.WriteLine("Q3. What have you got planned for the weekend? " + answer3);
+        }
+    }
+}
